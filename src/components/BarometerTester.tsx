c3a7b21dc9a6165c4f6f677fbbc4a07c50b988b3
RileyAlexis <MxRileyAlexis@gmail.com>
2024-06-02 11:21:31 -0500

    Added barometer and altimiter with metric and imperial

--- src/components/BarometerTester.tsx
+++ src/components/BarometerTester.tsx
@@ -4,14 +4,18 @@ import { Text } from '@ui-kitten/components';
 import { Barometer } from 'expo-sensors';
 
 export const BarometerTester: React.FC = () => {
-    const [pressure, setPressure] = useState<string | null>(null);
-    const [altitude, setAltitude] = useState<string | null>(null);
+    const [pressure, setPressure] = useState<number | null>(null);
+    const [altitudeMeters, setAltitudeMeters] = useState<number | null>(null);
+    const [altitudeFeet, setAltitudeFeet] = useState<number | null>(null);
+    const [inHg, setInHg] = useState<number | null>(null);
 
     useEffect(() => {
-        Barometer.addListener(({ pressure: any }) => {
+        Barometer.addListener(({ pressure }) => {
             setPressure(pressure);
-            const altitude = calculateAltitude(pressure);
-            setAltitude(altitude);
+            const altitudeMeters = calculateAltitude(pressure);
+            setAltitudeMeters(altitudeMeters);
+            setAltitudeFeet(metersToFeet(altitudeMeters))
+            setInHg(hPaToInHg(pressure));
         });
 
         return () => {
@@ -19,16 +23,28 @@ export const BarometerTester: React.FC = () => {
         }
     }, [])
 
-    const calculateAltitude = (pressure: any) => {
+    const calculateAltitude = (pressure: number) => {
         const seaLevelPressure = 1013.25;
         const altitude = (1 - Math.pow(pressure / seaLevelPressure, 0.190284)) * 145366.45 * 0.3048;
-        return altitude.toFixed(2);
+        return altitude;
+    }
+
+    const metersToFeet = (meters) => {
+        const feet = meters * 3.28084;
+        return feet;
+    }
+
+    const hPaToInHg = (hPa) => {
+        const inHg = hPa * 0.02953;
+        return inHg;
     }
 
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Pressure: {pressure ? `${pressure} hPa` : 'N/A'}</Text>
-            <Text style={styles.text}>Altitude: {altitude ? `${altitude} meters` : 'N/A'}</Text>
+            <Text style={styles.text}>Pressure: {pressure ? `${pressure.toFixed(4)} hPa` : 'N/A'}</Text>
+            <Text style={styles.text}>Altitude: {altitudeMeters ? `${altitudeMeters.toFixed(2)} meters` : 'N/A'}</Text>
+            <Text style={styles.text}>Altitude: {altitudeFeet ? `${altitudeFeet.toFixed(2)} feet` : 'N/A'}</Text>
+            <Text style={styles.text}>Altitude: {inHg ? `${inHg.toFixed(2)} inHg` : 'N/A'}</Text>
         </View>
     )
 }
