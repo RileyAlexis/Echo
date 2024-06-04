891faa000b1fa050cd4978182fdd3cbec5c8935a
RileyAlexis <MxRileyAlexis@gmail.com>
2024-06-04 09:14:51 -0500

    added react-native-canvas and planckJS

---
+++ src/components/PhysicsBox.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Canvas from 'react-native-canvas';
+import planck from 'planck-js';
+
+export const PhysicsBox: React.FC = () => {
+    return (
+        <>
+        </>
+    )
+}
